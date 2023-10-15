4aae16367a0147a7530e3a4d71c6ecc551f8c35c
Truls <trulstp@stud.ntnu.no>
2023-10-15 17:22:32 +0200

    added login, register, started on home

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,6 +1,30 @@
 import classes from "./Home.module.css";
 import React from "react";
 
-const Home = () => {};
+const Home = () => {
+  return (
+    <div>
+      <h1>DND AI</h1>
+      <div className={classes.container}>
+        <div className={classes.square}>
+          <img src='image1.jpg' alt='Image 1' />
+          <p>Text 1</p>
+        </div>
+        <div className={classes.square}>
+          <img src='image2.jpg' alt='Image 2' />
+          <p>Text 2</p>
+        </div>
+        <div className={classes.square}>
+          <img src='image3.jpg' alt='Image 3' />
+          <p>Text 3</p>
+        </div>
+        <div className={classes.square}>
+          <img src='image4.jpg' alt='Image 4' />
+          <p>Text 4</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
---
+++ frontend/src/Components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./Nav.module.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Encounters</li>
+        <li>Characters</li>
+        <li>Items</li>
+        <li>Spells</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
